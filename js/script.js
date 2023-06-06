d9162601813df20d286f0e137a3861e64b6b8343
Latipov Abdusamad <abdusamadlatipov05@gmail.com>
2023-06-06 06:33:21 +0300

    feat: edit

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 
 
 let arr = ["abdusamad", "latipov"]
+let str = "abdusamad"
 
 // function pop(arr) {
 //    let emptyArr = []
@@ -45,10 +46,17 @@ let arr = ["abdusamad", "latipov"]
 function join(arr, item) {
    let res = ''
    for(let i = 0; i < arr.length; i++){
-      res += arr[i]
-      res += item
+      res += arr[i] + item
    }
    return res
 }
 console.log(join(arr, " "));
 
+// function join(arr) {
+//    let res = []
+//    for(let i = 0; i < arr.length; i++){
+//       res[i] = arr[i]
+//    }
+//    return res
+// }
+// console.log(join(str));
